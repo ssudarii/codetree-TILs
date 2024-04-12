baa31dbcfc39c3269f2bb26cc4ac32cf2800b23a
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-12 09:45:28 +0900

    Title: 등급 매기기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240412/등급 매기기/ranking.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n >= 90 :
+    print("A")
+elif 90 > n >= 80 :
+    print("B")
+elif 80 > n >= 70 :
+    print("C")
+elif 70 > n >= 60 :
+    print("D")
+else : 
+    print("F")
