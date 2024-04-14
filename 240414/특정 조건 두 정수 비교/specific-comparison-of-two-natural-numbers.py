f48e70cb90fda2dc5d28e65ebe551ddb76eb4218
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-14 22:48:00 +0900

    Title: 특정 조건 두 정수 비교, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240414/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,14 @@
+a = input()
+arr = a.split()
+a = int(arr[0])
+b = int(arr[1])
+
+if a < b :
+    print(1, end=" ")
+else : 
+    print(0, end=" ")
+
+if a == b :
+    print(1, end=" ")
+else : 
+    print(0, end=" ")
