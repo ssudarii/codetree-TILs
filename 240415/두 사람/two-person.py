26ecd44fd8cdd1198a3aec9f5c335aaa5e1604b9
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-15 01:50:49 +0900

    Title: 두 사람, Time: 153ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240415/두 사람/two-person.py
@@ -0,0 +1,12 @@
+a = input()
+b = input()
+arr1 = a.split()
+arr2 = b.split()
+
+aa, ag = int(arr1[0]), arr1[1]
+ba, bg = int(arr2[0]), arr2[1]
+
+if ( aa >= 19 or ba >= 19 ) and ( ag == "M" or bg == "M" ) :
+    print("1")
+else :
+    print("0")
