48b4828ef085149f82b0b9fa498a38aef372c175
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-14 23:17:01 +0900

    Title: 비교 연산, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240414/비교 연산/comparison-operator.py
@@ -0,0 +1,34 @@
+a = input()
+arr = a.split()
+a = int(arr[0])
+b = int(arr[1])
+
+if a >= b :
+    print("1")
+else :
+    print("0")
+
+if a > b :
+    print("1")
+else :
+    print("0") 
+
+if b >= a :
+    print("1")
+else :
+    print("0")  
+
+if b > a :
+    print("1") 
+else :
+    print("0") 
+
+if a == b :
+    print("1")
+else :
+    print("0")
+
+if a != b :
+    print("1")
+else :
+    print("0")
