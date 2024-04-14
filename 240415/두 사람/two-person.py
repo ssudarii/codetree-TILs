b81cf5519274e90f4b62866cd7bf32656cf1f0d8
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-15 02:01:32 +0900

    Title: 두 사람, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240415/두 사람/two-person.py
+++ 240415/두 사람/two-person.py
@@ -6,7 +6,7 @@ arr2 = b.split()
 aa, ag = int(arr1[0]), arr1[1]
 ba, bg = int(arr2[0]), arr2[1]
 
-if ( aa >= 19 or ba >= 19 ) and ( ag == "M" or bg == "M" ) :
+if ( aa >= 19 and ag == "M" ) or ( ba >= 19 and bg == "M") :
     print("1")
 else :
     print("0")
