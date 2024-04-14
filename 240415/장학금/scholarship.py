e8d4d1cf3765f4dbf42271f28ac94a741249e3e4
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-15 01:36:07 +0900

    Title: 장학금, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240415/장학금/scholarship.py
@@ -0,0 +1,11 @@
+a = input()
+arr = a.split()
+m = int(arr[0])
+f = int(arr[1])
+
+if m >= 90 and f >= 95 :
+    print("100000")
+elif m >= 90 and 95 > f >= 90 : 
+    print("50000")
+else :
+    print("0")
