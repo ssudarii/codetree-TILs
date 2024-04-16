902391302bb2c4a5b38c46872806a44af00e42a6
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-16 17:58:26 +0900

    Title: 윤년 횟수 구하기, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240416/윤년 횟수 구하기/number-of-leap-years.py
+++ 240416/윤년 횟수 구하기/number-of-leap-years.py
@@ -4,6 +4,8 @@ cnt = 0
 for i in range(1, n+1):
     if i % 4 == 0 and i % 100 != 0 :
         cnt += 1
+    elif i % 400 == 0 :
+        cnt += 1
     elif i % 100 == 0 and i % 400 != 0 :
         pass
     else : 
