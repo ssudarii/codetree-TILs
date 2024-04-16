a52d6d9e7ca4d1a90453e08a11c8a8e17f2facc2
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-16 16:19:34 +0900

    Title: 369 게임, Time: 174ms, Memory: 25MB, Status: Passed - CodeTree

--- 240416/369 게임/369-game.py
+++ 240416/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(1, n+1):
-    if i % 3 == 0 or 30 <= i < 40 or 60 <= i < 70 or 90 <= i < 100 or (i % 10 == (3 or 6 or 9))  : 
+    if i % 3 == 0 or 30 <= i < 40 or 60 <= i < 70 or 90 <= i < 100 or i % 10 == 3 or i % 10 == 6 or i % 10 == 9 : 
         print("0", end=" ")
         i += 1
     else :
