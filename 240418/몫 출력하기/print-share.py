82ea9f9b473d808ab3376587db2e1fda2f9ea692
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-18 14:56:32 +0900

    Title: 몫 출력하기, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240418/몫 출력하기/print-share.py
+++ 240418/몫 출력하기/print-share.py
@@ -1,6 +1,10 @@
-for i in range(4):
+cnt = 0
+while True:
     n = int(input())
     if n % 2 != 0:
         pass
     elif n % 2 == 0:
         print(n // 2)
+        cnt += 1
+    if cnt == 3:
+        break
