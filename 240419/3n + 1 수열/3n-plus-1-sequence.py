76b52e26a8fd7aaad2239a2d10b8f2116ff74722
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-19 11:40:30 +0900

    Title: 3n + 1 수열, Time: 101ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240419/3n + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,18 @@
+N = int(input())
+cnt = 0
+
+while True:
+    if N % 2 == 0:
+        N = N // 2
+        cnt += 1
+        if N == 1:
+            break
+        
+    elif N % 2 != 0:
+        N = N * 3 + 1
+        cnt += 1
+        if N == 1:
+            break
+        
+
+print(cnt)
