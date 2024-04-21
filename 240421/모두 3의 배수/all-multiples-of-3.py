7f9abddb7b16d70e454db0e82d1d4ba83e8208d9
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-04-21 23:43:30 +0900

    Title: 모두 3의 배수, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240421/모두 3의 배수/all-multiples-of-3.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+if a % 3 == 0 and b % 3 == 0 and c % 3 == 0 and d % 3 == 0 and e % 3 == 0:
+    print(1)
+else:
+    print(0)
