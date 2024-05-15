fe6fcd4d03c51a0af291d36c930233a642e5d1a6
ssudarii <93572808+ssudarii@users.noreply.github.com>
2024-05-16 02:29:43 +0900

    Title: 규칙을 찾아 출력하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240516/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == 0 or i == n or j == n:
+            print("*", end=" ")
+        elif i > j:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
